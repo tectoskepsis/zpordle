6a128dc6b5aac69eb9401b046d3e2eb23400c3bf
tectoskepsis <copp.oscar0213@gmail.com>
2022-05-29 13:58:09 -0400

    clean up

--- zpordle.js
+++ zpordle.js
@@ -17,16 +17,10 @@ function entropy(guess, possibles, prime) {
   possibles.forEach( function (value) {
     d = norm_power(Math.abs(value - guess), prime)
     if (!dist.hasOwnProperty(d)) dist[d] = 0;
-    if (value == 0 || value == MAX_NUM) {
-      dist[d] += 0.5;
-    } else {
-      dist[d] += 1;
-    }
+    dist[d] += 1;
   });
-  if (guess == 1 && prime === 2) console.log(Object.values(dist));
   total = Object.values(dist).reduce((a, b) => a + b);
   ans = Object.values(dist).map((a) => Math.log2(total / a) * a / total).reduce((a, b) => a + b);
-  if (guess == 1 && prime === 2) console.log(ans);
   return ans;
 }
 
@@ -41,22 +35,6 @@ function price(possibles, prime) {
 function filter_numbers(guess, clue, prime) {
   numbers_left = numbers_left.filter((number) => norm_power(Math.abs(number - guess), prime) === clue);
 }
-
-// function sample_from_distribution(dist) {
-//   // should probably test this
-//   var tot = 0;
-//   for (var p in dist) {
-//     tot += dist[p];
-//   }
-//   var r = tot * Math.random();
-//   var sum = 0;
-//   for (var p in dist) {
-//     sum += dist[p];
-//     if (sum >= r) {
-//       return parseInt(p);
-//     }
-//   }
-// }
 
 function guess_helper(guess, prime) {
   var li = document.createElement("li");
@@ -76,9 +54,7 @@ function guess_helper(guess, prime) {
   li.style.backgroundColor = get_color(pow);
   share_emojis.push(pow);
   document.getElementById("guesses").appendChild(li);
-  // guesses++;
   if (val != 0) {
-    // document.getElementById("curguess").innerHTML = "Current Prime: " + todays_primes[guesses];
     filter_numbers(guess, pow, prime);
     MY_PRIMES.forEach(function (p) {
       document.getElementById(p+"-price").innerHTML = "¥" + price(numbers_left, p);
@@ -87,122 +63,13 @@ function guess_helper(guess, prime) {
   }
   won = (val == 0);
 
-  // update statistics
-  if (localStorage.getItem("last-played-date") != today) {
-    // update guess statistics
-    var statistics = JSON.parse(localStorage.getItem("statistics"));
-    statistics[won ? guesses : 0]++;
-    localStorage.setItem("statistics", JSON.stringify(statistics));
-    // update streak statistics
-    var streaks = JSON.parse(localStorage.getItem("streaks"));
-    if (won) {
-      streaks["current-streak"]++;
-      streaks["max-streak"] = Math.max(streaks["current-streak"], streaks["max-streak"]);
-    } else {
-      streaks["current-streak"] = 0;
-    }
-    localStorage.setItem("streaks", JSON.stringify(streaks));
-    //
-    localStorage.setItem("last-played-date", today)
-  }
-
-  // Game is over (either guess was correct, or we're out of guesses).
+  // Game is over
   var result_string = val == 0 ?
     "You win!" :
     "You lose. Today's number was " + target + ".";
   document.getElementById("button").disabled = true;
-  // document.getElementById("curguess").innerHTML = "";
 
-  displayStats(result_string, /* includeShare= */true);
-}
-
-function stats() {
-  displayStats("", false);
-}
-
-function displayStats(result_string, includeShare) {
-  result_string += "<canvas id=\"stats\" style=\"height: 300px; width: 100%;\"></canvas>";
-  result_string += "<div id=\"streak\" style=\"display: flex; color: black;\"></div>";
-  if (includeShare) {
-    result_string += "<br/>" + SHARE_BUTTON;
-  }
-  document.getElementById("result").innerHTML = result_string;
-
-  var statistics = JSON.parse(localStorage.getItem("statistics"));
-  var streaks = JSON.parse(localStorage.getItem("streaks"));
-  var games_played = statistics.reduce((a, b) => a + b)
-  var wins = statistics.slice(1).reduce((a, b) => a + b)
-  var winpercent = (games_played == 0) ? "N/A" : Math.floor(100 * wins / games_played) + "%";
-  var streak_string = "";
-  streak_string += "<div id=\"played\" style=\"flex: 1;\">Played<div style=\"font-size: 2em\">" + games_played + "</div></div>";
-  streak_string += "<div id=\"winpercent\" style=\"flex: 1\">Win Percentage<div style=\"font-size: 2em\">" + winpercent + "</div></div>";
-  streak_string += "<div id=\"curstreak\" style=\"flex: 1\">Current Streak<div style=\"font-size: 2em\">" + streaks["current-streak"] + "</div></div>";
-  streak_string += "<div id=\"maxstreak\" style=\"flex: 1\">Max Streak<div style=\"font-size: 2em\">" + streaks["max-streak"] + "</div></div>";
-  document.getElementById("streak").innerHTML = streak_string;
-
-  loadStats();
-  $('#result').modal('show');
-}
-
-function loadStats() {
-  var statistics = JSON.parse(localStorage.getItem("statistics"));
-  //
-  const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const data = {
-    labels: labels,
-    datasets: [{
-      barPercentage: .9,
-      minBarLength: 20,
-      data: statistics.slice(1)
-    }]
-  };
-  Chart.register(ChartDataLabels);
-  const config = {
-    plugins: [ChartDataLabels],
-    type: 'bar',
-    data: data,
-    options: {
-      indexAxis: "y",
-      scales: {
-        x: {
-          display: false
-        }
-      },
-      plugins: {
-        legend: {
-          display: false
-        },
-
-        title: {
-          text: "Guess Distribution",
-          display: true
-        },
-        tooltip: {
-          enabled: false
-        },
-        datalabels: {
-          anchor: "end",
-          align: "left",
-          // only display label if > 0
-          display: function (context) {
-            var index = context.dataIndex;
-            var value = context.dataset.data[index];
-            return value > 0;
-          }
-        }
-      },
-    }
-  };
-
-  const myChart = new Chart(document.getElementById("stats"), config);
-}
-
-function loadStreak() {
-  var elt = document.getElementById("streak");
-  var streaks = JSON.parse(localStorage.getItem("streaks"));
-  var streak_string = "This will hopefully look better soon.<br>";
-  streak_string += "Streak: " + JSON.stringify(streaks);
-  elt.innerHTML = streak_string;
+  document.getElementById("share-button").style.display = "block";
 }
 
 function guess() {
@@ -229,7 +96,7 @@ function guess() {
 function share() {
   // need to make the display date a separate variable because 'today' is used for seeding the random number generation.
   var today_but_not_weird = (nd.getMonth() + 1) + '/' + nd.getDate() + '/' + nd.getFullYear();
-  var text = "I solved the " + today_but_not_weird + " Zpordle " + "with ¥" + spent + ".\n";
+  var text = "I solved the " + today_but_not_weird + " Zpordle Shop " + "by spending ¥" + spent + ".\n";
   var emojis = "";
   for (var i = 0; i < share_emojis.length; i++) {
     emojis += emoji_lookup(share_emojis[i]);
@@ -292,21 +159,10 @@ function updateDarkMode() {
 // constants
 var MAX_NUM = 1000;
 var NUM_PRIMES = 10;
-// var NUM_GUESSES = 10;
-//
-// document.getElementById("max-guess").innerHTML = MAX_NUM;
-// document.getElementById("num-guesses").innerHTML = NUM_GUESSES;
-//
 const PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227];
 const MY_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 // these are expensive to compute so store them
 const BASE_PRICES = {2: 500, 3: 346, 5: 226, 7: 174, 11: 122, 13: 107, 17: 87, 19: 80, 23: 69, 29: 58}
-// var DISTRIBUTION = {}
-// for (var i = 0; i < MY_PRIMES.length; i++) {
-//   var p = MY_PRIMES[i];
-//   DISTRIBUTION[p] = 1.0 / p;
-// }
-//
 const EMOJI_TABLE = {
   "-1": String.fromCodePoint(0x2611),
   "0": String.fromCodePoint(0x1F7E5),
@@ -314,7 +170,6 @@ const EMOJI_TABLE = {
   "2": String.fromCodePoint(0x1F7E8),
   "3": String.fromCodePoint(0x1F7E9),
 }
-var SHARE_BUTTON = "<button id=\"share\" type=\"button\" onclick=\"share()\">Share</button>";
 
 // always use pacific time
 var d = new Date();
@@ -322,43 +177,34 @@ var pstDate = d.toLocaleString("en-us", {
   timeZone: "America/Los_Angeles"
 });
 var nd = new Date(pstDate);
-var today = nd.getFullYear() + '/' + (nd.getMonth() + 1) + '/' + nd.getDate();
+var today = nd.getFullYear() + "" + (nd.getMonth() + 1) + "" + nd.getDate();
 
-// using https://github.com/davidbau/seedrandom
-Math.seedrandom(today);
+// taken from https://stackoverflow.com/a/52171480
+const cyrb53 = function(str, seed = 0) {
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1>>>0);
+};
 
-var target = Math.round(Math.random() * MAX_NUM);
+var target = cyrb53(today) % (MAX_NUM + 1);
 var numbers_left = Array.from(Array(MAX_NUM).keys())
 // var todays_primes = []
 var guesses = 0;
 var won = false;
 var spent = 0;
 var share_emojis = [];
-
-// for (var i = 0; i < NUM_GUESSES; i++) {
-//   todays_primes.push(sample_from_distribution(DISTRIBUTION));
-// }
-// todays_primes.sort(function (a, b) {
-//   return a - b;
-// });
-
-// document.getElementById("info").innerHTML = "Today's Primes: " + todays_primes.join(", ");
-// document.getElementById("curguess").innerHTML = "Current Prime: " + todays_primes[0];
+document.getElementById("share-button").style.display = "none";
 
 MY_PRIMES.forEach(function (prime) {
   document.getElementById(prime+"-price").innerHTML = "¥" + BASE_PRICES[prime.toString()];
 });
 
-// initialize statistics/streaks if we haven't yet
-// if (localStorage.getItem("statistics") === null) {
-//   localStorage.setItem("statistics", JSON.stringify(new Array(11).fill(0)));
-// }
-if (localStorage.getItem("streaks") === null) {
-  localStorage.setItem("streaks", JSON.stringify({
-    "current-streak": 0,
-    "max-streak": 0
-  }));
-}
 if (localStorage.getItem("dark-mode") === null) {
   localStorage.setItem("dark-mode", "false");
 } else {
@@ -376,21 +222,8 @@ if (localStorage.getItem("date") != today) {
     guess_helper(arr[i][0], arr[i][1]);
   }
 }
-// check if we missed a day
-var streaks = JSON.parse(localStorage.getItem("streaks"));
-try {
-  var last_played = localStorage.getItem("last-played-date");
-  if (Date.parse(today) - Date.parse(last_played) > 86400000) {
-    streaks["current-streak"] = 0;
-  }
-} catch {
-  // if last-date-played does not exist
-  streaks["current-streak"] = 0;
-}
-localStorage.setItem("streaks", JSON.stringify(streaks));
 // Shamelessly stolen from w3schools like a proper programmer.
-var guess_input = document.getElementById("guess-input");
-guess_input.addEventListener("keyup", function (event) {
+document.getElementById("guess-input").addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("button").click();
